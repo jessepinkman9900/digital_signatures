367081ed5075434fcd114ddc1b9127783f398bed
srinivas_kota <saisrinivasa.likhit@students.iiit.ac.in>
2019-04-08 20:15:29 +0530

    minor

--- digital_signatures/routes.py
+++ digital_signatures/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from digital_signatures import digital_signatures
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
